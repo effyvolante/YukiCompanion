72a67db286d9230b073309e56f7b844a475a8b4c
Jay Bracewell <jaybracewell@Mac-mini.local>
2026-09-06 15:34:46 -0700

    Add Windows WPF entry point

--- Windows/Program.cs
+++ Windows/Program.cs
@@ -4,6 +4,9 @@ namespace YukiCompanion.Windows;
 
 public partial class App : Application
 {
+    [STAThread]
+    public static void Main() => new App().Run();
+
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
