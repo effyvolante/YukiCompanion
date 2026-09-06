f5a2e7540c9cd70427b6aadbd45365225b58567b
Jay Bracewell <jaybracewell@Mac-mini.local>
2026-09-06 15:36:13 -0700

    Fix Windows bridge test imports

--- Windows.Tests/ChromeBridgeClientTests.cs
+++ Windows.Tests/ChromeBridgeClientTests.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace YukiCompanion.Windows.Tests;
