5810f1b16e63d746ef438cd2c015edcb2555e055
Jay Bracewell <jaybracewell@Mac-mini.local>
2026-09-07 19:12:30 -0700

    Set corrective release version to 1.0.3

--- Sources/EffyWoWCompanion/UpdateService.swift
+++ Sources/EffyWoWCompanion/UpdateService.swift
@@ -4,7 +4,7 @@ import Foundation
 @MainActor
 final class UpdateService {
     static let shared = UpdateService()
-    static let currentVersion = "1.0.2"
+    static let currentVersion = "1.0.3"
     static let repositoryURL = URL(string: "https://github.com/effyvolante/YukiCompanion")!
     private static let releasesAPI = URL(string: "https://api.github.com/repos/effyvolante/YukiCompanion/releases/latest")!
 
